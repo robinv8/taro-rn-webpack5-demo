759fdb74e3a59e713d79a5b61445fe16e3e713d6
robin <robin@rnode.me>
2024-01-13 23:14:37 +0800

    fix https://github.com/NervJS/taro-ui/issues/1746

--- config/index.ts
+++ config/index.ts
@@ -28,6 +28,9 @@ const config = {
       exclude: ['taro-ui']
     }
   },
+  sass: {
+    resource: ["src/styles/common.scss", "src/styles/common.scss", "src/styles/common.scss", "src/styles/common.scss"],
+  },
   cache: {
     enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
